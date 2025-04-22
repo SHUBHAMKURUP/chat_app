febbf26d414cffc721f6c76b870cb638a0b6fe23
shubham <shubham3325@gmail.com>
2025-04-22 14:15:37 +0530

    feat: implement messaging functionality with message routes, model, and controller

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 // Initialize express once
@@ -22,6 +23,7 @@ app.use(cookieParser());
 //   })
 // );
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 // Server Setup
 // const server = http.createServer(app);
