93d7b8e901d6dae58a931f40663e8b3f0807525e
shubham <shubham3325@gmail.com>
2025-04-24 02:01:54 +0530

    feat: add lucide-react for icons and implement authentication state management with useAuthStore

--- server/src/index.js
+++ server/src/index.js
@@ -16,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // app.use(
 //   cors({
 //     origin: "http://localhost:3000",
